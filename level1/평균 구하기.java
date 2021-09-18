1aab9006b9bc690c426e3c9d5bd38a9ead864f79
Sharpie <71365547+sharpie1330@users.noreply.github.com>
2021-09-19 06:01:49 +0900

    Create 평균 구하기.java

---
+++ level1/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+        for(int a : arr)
+            sum += a;
+        double answer = (double)sum/arr.length;
+        return answer;
+    }
+}
